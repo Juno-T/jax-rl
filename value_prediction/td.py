43c8085a6ece95de46f6ad19f02c70ef9aab93d9
Juno-T <yossathorn.tian@gmail.com>
2022-03-08 20:00:40 +0900

    make jit default

--- value_prediction/td.py
+++ value_prediction/td.py
@@ -1,15 +1,22 @@
 import numpy as np
 import jax
 import jax.numpy as jnp
+from functools import partial
 
 def td_zero_error(v_tm1, r_t, v_t, discount):
   return r_t+discount*v_t - v_tm1
 
-def nstep_td_errors(n, v_tm1, r_t, v_t, discount):
+@partial(jax.jit, static_argnums=(0,))
+def nstep_td_errors(n : int, v_tm1, r_t, v_t, discount):
   """
     G(n,t) = rt + gamma*G(n-1, t+1)
     G(1,t) = rt + gamma*v(t+1)
   """
+  assert(n>0)
+  assert(v_tm1.shape == r_t.shape)
+  assert(v_t.shape == r_t.shape)
+  assert(v_tm1.shape[0]>=n)
+  # todo: discount_t
   targets = v_t[n-1:]
   for i in range(n):
     targets = r_t[n-1-i:len(r_t)-i] + targets * discount
