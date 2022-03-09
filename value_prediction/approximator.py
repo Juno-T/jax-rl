3eb03e1fa8a0ded2de74e7b890102dec6923a96a
Juno-T <yossathorn.tian@gmail.com>
2022-03-09 13:07:22 +0900

    add td lambda, add tab appr update

--- value_prediction/approximator.py
+++ value_prediction/approximator.py
@@ -5,7 +5,7 @@ import jax.numpy as jnp
 
 class tabularApproximator:
   def __init__(self, state_range):
-    self.state_range = [jnp.sort(jnp.array(r)) for r in state_range]
+    self._state_range = [jnp.sort(jnp.array(r)) for r in state_range]
     self.table = jnp.zeros([len(r) for r in state_range], dtype=jnp.float32)
 
 
@@ -19,12 +19,29 @@ class tabularApproximator:
   def v(self, state):
     """
     """
-    assert(len(state) == len(self.state_range))
+    assert(len(state) == len(self._state_range))
     return self.table[tuple(self.indexof(state))]
 
   def indexof(self, state):
     idx = []
     for i in range(len(state)):
-      idx.append(jnp.searchsorted(self.state_range[i], state[i]))
+      idx.append(jnp.searchsorted(self._state_range[i], state[i]))
     return idx
+
+  def update(self, state, value):
+    self.table = self.table.at[tuple(self.indexof(state))].set(value)
+    return 0
+
+  
+
+  def batch_update(self, states, values):
+    def _batch_update_table(table, states, values):
+      states = jnp.array(states)
+      values = jnp.array(values)
+      def _update_table(table, x):
+        return table.at[tuple(self.indexof(x[:-1]))].set(x[-1]), None
+      table, ys = jax.lax.scan(_update_table, \
+        table, jnp.concatenate((states, jnp.expand_dims(values, axis=1)),axis=1))
+      return table
+    self.table = jax.jit(_batch_update_table)(self.table, states, values)
 
--- value_prediction/td.py
+++ value_prediction/td.py
@@ -7,18 +7,35 @@ def td_zero_error(v_tm1, r_t, v_t, discount):
   return r_t+discount*v_t - v_tm1
 
 @partial(jax.jit, static_argnums=(0,))
-def nstep_td_errors(n : int, v_tm1, r_t, v_t, discount):
+def nstep_td_errors(n : int, v_tm1, r_t, v_t, discount_t):
   """
-    G(n,t) = rt + gamma*G(n-1, t+1)
-    G(1,t) = rt + gamma*v(t+1)
+    G(n,t) = r(t+1) + gamma*G(n-1, t+1)
+    G(1,t) = r(t+1) + gamma*v(t+1)
   """
+  discount_t = discount_t * jnp.ones_like(r_t) # if scalar, to array
   assert(n>0)
   assert(v_tm1.shape == r_t.shape)
   assert(v_t.shape == r_t.shape)
-  assert(v_tm1.shape[0]>=n)
-  # todo: discount_t
+  assert(r_t.shape[0]>=n)
+
   targets = v_t[n-1:]
   for i in range(n):
-    targets = r_t[n-1-i:len(r_t)-i] + targets * discount
+    targets = r_t[n-1-i:len(r_t)-i] + targets * discount_t[n-1-i:len(discount_t)-i]
   
   return targets-v_tm1[:len(v_tm1)-n+1]
+
+@partial(jax.jit, static_argnums=(0,))
+def td_lambda_errors(lmd, v_tm1, r_t, v_t, discount_t):
+  """
+    G(n,t) = r(t+1) + gamma((1-lmd)v(t+1)+lmdG(t+1))
+  """
+  discount_t = discount_t * jnp.ones_like(r_t) # if scalar, to array
+  assert(v_tm1.shape == r_t.shape)
+  assert(v_t.shape == r_t.shape)
+
+  G_t = v_t[-1]
+  returns=[]
+  for i in range(v_tm1.shape[0]-1,-1,-1):
+    G_t = r_t[i]+discount_t[i]*((1-lmd)*v_t[i] + lmd*G_t)
+    returns.append(G_t)
+  return jnp.array(returns)[::-1]-v_tm1
